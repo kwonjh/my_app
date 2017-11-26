674add954555e390aeebb8d0bc5a2a016d47b6a0
helloworld <apoptosis_1@naver.com>
2017-11-26 18:18:30 +0900

    biggerLetter

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app=express();
 
 app.use(express.static(__dirname+'/public'));
-
+console.log(__dirname);
 app.listen(3000,function(){
   console.log('Server on!');
 });
